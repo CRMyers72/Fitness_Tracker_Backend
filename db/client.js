e82c929668b0a9594aa59da74b5a9dc2885f1e2b
CRMyers72 <91919116+CRMyers72@users.noreply.github.com>
2022-08-10 19:53:44 -0400

    updating client

--- db/client.js
+++ db/client.js
@@ -1,8 +1,8 @@
 // build and export your unconnected client here
 const { Client } = require('pg')
-
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/fitness_dev'
 const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/fitness_dev',
+  connectionString,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 })
 
--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json)
 const morgan = require('morgan')
 app.use(morgan('dev'))
 
+const cors = require('cors')
+app.use(cors)
+
 app.use((req, res, next) =>{
     console.log(req.body)
     next()
@@ -19,6 +22,7 @@ const apiRouter = require('./api');
 app.use('/api', apiRouter);
 
 const { client } = require('./db/client')
+console.log(client)
 client.connect();
 
 app.get('/', function (req, res) {
